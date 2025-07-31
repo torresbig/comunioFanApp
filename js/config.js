f02f0c885afaf775902eec1d19977f12983dd931
ToRReS BiG <78181335+torresbig@users.noreply.github.com>
2025-08-01 00:14:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,11 +6,11 @@ const GITHUB_USER = "torresbig";
 const GITHUB_REPO = "comunioFanApp";
 
 const DATA_URLS = {
-  clubs: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/VereinsdatenbankJson.txt`,
-  players: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/SpielerdatenbankNeutralJson.txt`,
-  users: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/UserdatenbankJson_884691.txt`,
-  playerToUser: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/PlayerToUserMap_884691.txt`,
-  news: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/News.json`
+  clubs: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/data/VereinsdatenbankJson.txt`,
+  players: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/data/SpielerdatenbankNeutralJson.txt`,
+  users: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/data/UserdatenbankJson_884691.txt`,
+  playerToUser: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/data/PlayerToUserMap_884691.txt`,
+  news: `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main/data/News.json`
 };
 
 
