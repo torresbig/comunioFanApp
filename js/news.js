9e7c6fdc5d15ad2f578e0891d653a3e9e88148d9
ToRReS BiG <78181335+torresbig@users.noreply.github.com>
2025-08-23 13:58:54 +0000

    Update news.js

--- js/news.js
+++ js/news.js
@@ -57,7 +57,7 @@ function renderNews(newsList) {
                                 const statusDetail = match[4] || '';
                                 let statusDisplay = `<b>${status.replace(/_/g, ' ')}</b>`;
                                 if (statusDetail) statusDisplay += ` (${statusDetail})`;
-                 if(status ==="ACTIV") {
+                 if(statusDisplay ==="ACTIV") {
 
                                 text = `🟢 ${linkPlayer(news.playerId, playerName)} ist ${match[2]} ${statusDisplay}`; 
                  } else{
@@ -137,4 +137,5 @@ function linkPlayer(playerId, playerName) {
     return `<a href="${url}" style="color:#80f; font-weight:bold;">${playerName}</a>`;
 
 }
+
 
